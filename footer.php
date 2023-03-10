2fca2be292ca32c87e691c9820941d2fd5bf2d41
Joseph Giron <jgiron@ucf.edu>
2023-03-09 19:30:01 -0500

    initial commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+			</div>
+		</main>
+		<?php echo ucfwp_get_footer_markup(); ?>
+		<?php wp_footer(); ?>
+		<!-- DO NOT EDIT BELOW THIS LINE -->
+		<!-- Mainstay Web Chat -->
+		<script>window.admitHubBot = {botToken: "67ec49b5e62348d28", collegeId: "universityCentralFlorida"};</script>
+		<script async src=https://webbot.mainstay.com/static/js/webchat.js></script>
+		<link rel="stylesheet" type="text/css" href=https://webbot.mainstay.com/static/css/webchat.css>
+		<!-- DO NOT EDIT ABOVE THIS LINE -->
+	</body>
+</html>
